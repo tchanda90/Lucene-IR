1529bc68f41ca6ca6a10f0b4d721a71975fc4e30
tchanda90 <tchanda90@gmail.com>
2017-12-10 20:32:15 +0100

    final commit

--- src/ovgu/ir/rant/Main.java
+++ src/ovgu/ir/rant/Main.java
@@ -7,30 +7,32 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		// Check if cmd line arguments are provided
-		if (args.length != 4) {
+		// Check if cmd line arguments are empty
+		if (!args[0].equals("") && !args[1].equals("") && !args[2].equals("") && !args[3].equals("")) {
+			
+			// Get docs path, index path, ranking mode, and query string from cmd line arguments
+			String docsPath = args[0];
+			String indexPath = args[1];
+			String rankingModel = args[2];
+			String query = args[3];
+
+
+			// Run the indexer and pass the path to the docs and index folders
+			IndexFiles.index(docsPath, indexPath);
+
+			// Run the searcher and pass the path to the index folder, query string, and ranking model
+			try {
+				SearchFiles.search(indexPath, query, rankingModel);
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+		}
+		else {
 			System.out.print("Invalid command line arguments. Must be run as follows:\n");
 			System.out.print("java -jar IR_P01.jar [path to document folder] [path to index folder] [VS/OK] [query]\n" + "");
 			return;
-		}
-
-		// Get docs path and index path from cmd line arguments
-		String docsPath = args[0];
-		String indexPath = args[1];
-		String rankingModel = args[2];
-		String query = args[3];
-
-
-		// Run the indexer and pass the path to the docs and index folders
-		IndexFiles.index(docsPath, indexPath);
-
-		// Run the searcher and pass the path to the index folder, query string, and ranking model
-		try {
-			SearchFiles.search(indexPath, query, rankingModel);
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (ParseException e) {
-			e.printStackTrace();
 		}
 	}
 
